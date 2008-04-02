529232bc1e277e6538b072b5b9d12b7e68d1dedc
patrice <patrice@radicaldesigns.org>
2008-04-02 11:55:18 -0700

    tweaking controller to get xml feed to work.

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,7 +1,20 @@
 class Admin::ReportsController < AdminController
+#=begin
   active_scaffold :report do |config|
     config.list.columns = [:created_at, :phone, :voice_mail, :city, :state, :file_status]
     config.update.columns = [:voice_mail, :created_at, :phone, :city, :state, :voice_mail, :notes]
     config.list.sorting = {:created_at => :desc}
+  	config.actions.exclude :create
   end
+#=end
+
+=begin
+  def list
+    raise
+    @reports = Campaign.reports
+    respond_to do |format|
+      format.xml {render :xml => @reports.to_xml}
+    end
+  end
+=end
 end
