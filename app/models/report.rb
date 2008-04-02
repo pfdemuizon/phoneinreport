28d3037103e8f588cc5d1c15e7cbbc5bcdd0648b
patrice <patrice@radicaldesigns.org>
2008-04-02 11:56:09 -0700

    clean up branch.  On branch master 	modified:   app/models/report.rb 	modified:   config/amazon_s3.yml 	modified:   config/environment.rb 	modified:   config/routes.rb 	modified:   doc/README_FOR_APP 	modified:   doc/gp_phone.xls

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,10 @@
 class Report < ActiveRecord::Base
   belongs_to :campaign
   has_one :voice_mail
-  #acts_as_mappable 
+#  acts_as_mappable :auto_geocode => {:field => :city_state, :error_message => 'Could not geocode address'}, 
+#      :lat_column_name => 'latitude', :lng_column_name => 'longitude'
+
+  def city_state
+    [@city, @state].join(', ')
+  end
 end
