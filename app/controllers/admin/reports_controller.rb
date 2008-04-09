49d8487237b1299c0f9f2ca7e8dd44fce55e4ec3
patrice <pfdemuizon@gmail.com>
2008-04-09 00:10:05 -0700

    replace state_select plugin with our own state select code (from daysofaction).

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ReportsController < AdminController
  
   active_scaffold :report do |config|
-    config.list.columns = [:created_at, :phone, :voice_mail, :city, :state, :file_status]
+    config.list.columns = [:created_at, :phone, :voice_mail, :latitude, :longitude, :city, :state, :file_status]
     config.update.columns = [:voice_mail, :created_at, :phone, :city, :state, :voice_mail, :notes]
     config.list.sorting = {:created_at => :desc}
   	config.actions.exclude :create
--- app/helpers/admin/reports_helper.rb
+++ app/helpers/admin/reports_helper.rb
@@ -1,25 +1,26 @@
 module Admin::ReportsHelper
-  def voice_mail_column(report)
+
+  def mp3_player(report)
     if report.voice_mail
-      controller.send(
-        :render_to_string, 
-        :partial => "mp3_player", 
-        :locals => {:id => report.voice_mail.id, :file => report.voice_mail.public_filename}
-      )
+      controller.send(:render_to_string, :partial => "mp3_player", 
+          :locals => {:id => report.voice_mail.id, 
+          :file => report.voice_mail.public_filename})
     end
   end
 
+  def voice_mail_column(report)
+    mp3_player(report)
+  end
+
   def voice_mail_form_column(report, input_name)
-    if report.voice_mail
-      controller.send(
-        :render_to_string, 
-        :partial => "mp3_player", 
-        :locals => {:id => report.voice_mail.id, :file => report.voice_mail.public_filename}
-      )
-    end
+    mp3_player(report)
   end
   
-  def state_form_column(report, input_name)
-    state_select(report, input_name)
+  def state_form_column(report, state)
+    select(report, state, StateSelect.state_options_for_select, {:include_blank => true})
+  end
+
+  def created_at_form_column(report, input_name)
+    report.created_at.strftime("%m/%d/%Y %I:%M%p")
   end
 end
