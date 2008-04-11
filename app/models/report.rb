2680656a76e5907f791daef6cd8daea8f588155f
patrice <pfdemuizon@gmail.com>
2008-04-11 12:37:26 -0700

    lets get this working with s3.  also have mail_fetcher check all site/campaign inboxes

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,16 +1,16 @@
 class Report < ActiveRecord::Base
   belongs_to :campaign
-  has_one :voice_mail
+  has_one :voice_mail, :dependent => :destroy
   has_one :reporter, :class_name => 'User'
   acts_as_mappable 
   before_validation :geocode_address
- 
   def address
-    [self.city, self.state].join(', ')
+    (self.city and self.state) ? [self.city, self.state].join(', ') : nil
   end
 
 protected
   def geocode_address
+    return unless address  # avoid unnecessary geocode if address not set
     geo = GeoKit::Geocoders::MultiGeocoder.geocode(address)
     errors.add(:address, "Could not Geocode address") if !geo.success
     self.latitude, self.longitude = geo.lat, geo.lng if geo.success
--- app/models/voice_mail.rb
+++ app/models/voice_mail.rb
@@ -1,9 +1,9 @@
 class VoiceMail < ActiveRecord::Base
   belongs_to :report
-  if RAILS_ENV == "test"
-    has_attachment :storage => :file_system, :content_type => ['audio/wav', 'audio/mp3'], :processor => :mp3_encoder
-  else
+  if RAILS_ENV == "production" or RAILS_ENV == "development"
     has_attachment :storage => :s3, :content_type => ['audio/wav', 'audio/mp3'], :processor => :mp3_encoder
+  else
+    has_attachment :storage => :file_system, :content_type => ['audio/wav', 'audio/mp3'], :processor => :mp3_encoder
   end
   validates_as_attachment
 end
