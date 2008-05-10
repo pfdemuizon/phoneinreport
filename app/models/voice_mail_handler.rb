d09470a7229ce3c855bb22b662053a1f76318bc3
patrice <pfdemuizon@gmail.com>
2008-05-10 10:58:24 -0700

    add my own logger to voice mail handler.

--- app/models/voice_mail_handler.rb
+++ app/models/voice_mail_handler.rb
@@ -1,18 +1,19 @@
 class VoiceMailHandler < ActionMailer::Base
+  dump = Logger.new(File.join(RAILS_ROOT,'log', 'mail_errors.log'))
   def receive(email)
-    logger.info("Processing email...")
+    dump.info("Processing email...")
     @campaign = Campaign.find_by_email(email.to.first)
     unless @campaign
-      logger.warn("Could not find a campaign for #{email.to}.")
+      dump.warn("Could not find a campaign for #{email.to}.")
       return 
     end
     @report = @campaign.reports.build
     unless @voice_mail = VoiceMail.create(:email => email)
-      logger.warn("Error saving voice mail: #{@voice_mail.errors}")
+      dump.warn("Error saving voice mail: #{@voice_mail.errors}")
     end
     @report.voice_mail = @voice_mail if @voice_mail
     unless @report.save
-      logger.warn("Report errors: #{@report.errors}") 
+      dump.warn("Report errors: #{@report.errors}") 
     end
   end
 end
